8280d4ec4a60b0a1bc1e152963aedf60eb888813
Raymondl177 <raymondl177@nycstudents.net>
2025-10-08 12:32:34 -0400

    e

---
+++ shop.js
@@ -0,0 +1,144 @@
+const landmarks = [
+  {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+  {
+    name: "Eiffel Tower",
+    price: "2,500,000,000",
+    inStock: true,
+    continent: "Europe",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Great Wall of China",
+    price: "50,000,000,000",
+    inStock: true,
+    continent: "Asia",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Taj Mahal",
+    price: "10,000,000,000",
+    inStock: true,
+    continent: "Asia",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Statue of Liberty",
+    price: "3,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Colosseum",
+    price: "1,200,000,000",
+    inStock: true,
+    continent: "Europe",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Machu Picchu",
+    price: "6,000,000,000",
+    inStock: true,
+    continent: "South America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Sydney Opera House",
+    price: "2,000,000,000",
+    inStock: true,
+    continent: "Australia",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Big Ben",
+    price: "5,000,000,000",
+    inStock: true,
+    continent: "Europe",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Pyramids of Giza",
+    price: "30,000,000,000",
+    inStock: true,
+    continent: "Africa",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Washington Monument",
+    price: "3,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "Leaning Tower of Pisa",
+    price: 1,000,000,000,
+    inStock: true,
+    continent: "Europe",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  },
+    {
+    name: "White house",
+    price: "67,000,000,000",
+    inStock: true,
+    continent: "North America",
+    img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
+  }
+];
+
+landmarks.forEach((landmark) => console.log(landmark.continent))
