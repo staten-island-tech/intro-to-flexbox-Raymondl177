c16bb835d68d1d40867cc31b65fd69aa6dbf0a24
Raymondl177 <raymondl177@nycstudents.net>
2025-10-09 12:34:32 -0400

    d

--- shop.js
+++ shop.js
@@ -1,144 +1,166 @@
 const landmarks = [
   {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
   {
     name: "Eiffel Tower",
-    price: "2,500,000,000",
+    price: 2500000000,
     inStock: true,
     continent: "Europe",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Great Wall of China",
-    price: "50,000,000,000",
+    price: 50000000000,
     inStock: true,
     continent: "Asia",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Taj Mahal",
-    price: "10,000,000,000",
+    price: 10000000000,
     inStock: true,
     continent: "Asia",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Statue of Liberty",
-    price: "3,000,000,000",
+    price: 3000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Colosseum",
-    price: "1,200,000,000",
+    price: 1200000000,
     inStock: true,
     continent: "Europe",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Machu Picchu",
-    price: "6,000,000,000",
+    price: 6000000000,
     inStock: true,
     continent: "South America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Sydney Opera House",
-    price: "2,000,000,000",
+    price: 2000000000,
     inStock: true,
     continent: "Australia",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Big Ben",
-    price: "5,000,000,000",
+    price: 5000000000,
     inStock: true,
     continent: "Europe",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Pyramids of Giza",
-    price: "30,000,000,000",
+    price: 30000000000,
     inStock: true,
     continent: "Africa",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Washington Monument",
-    price: "3,000,000,000",
+    price: 3000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "Leaning Tower of Pisa",
-    price: 1,000,000,000,
+    price: 1000000000,
     inStock: true,
     continent: "Europe",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   },
     {
     name: "White house",
-    price: "67,000,000,000",
+    price: 67000000000,
     inStock: true,
     continent: "North America",
     img: "https://media.istockphoto.com/id/147646926/photo/the-white-house.jpg?s=612x612&w=0&k=20&c=ONA6w_1XbCbTD2QGL8qMgVZtEgcn4TIGeXQEtGm5Fh0="
   }
 ];
 
-landmarks.forEach((landmark) => console.log(landmark.continent))
+/* landmarks.forEach((landmark) => console.log(landmark.continent)) */
+
+//create inject function
+function inject(landmark) {
+//do something
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML("afterbegin", 
+    `<div class="card">
+      <img class="card-image" src=${landmark.img} alt=${landmark.name}/>
+      <h1 class="card-title"> ${landmark.name}</h1>
+      <h2 class="card-price">${landmark.price}</h2>
+      <h3 class="card-continent">${landmark.continent}</h3>
+      <button class="cart-button">Add to Cart</button>
+    </div>`
+  );
+
+  //query the container
+  //using adjacent html push card into container
+}
+
+
+landmarks.forEach((landmark) => inject(landmark));
+//loop through items
